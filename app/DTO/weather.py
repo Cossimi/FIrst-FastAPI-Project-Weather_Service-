9984f96406ff2c45709fb99edac3ba69d0572220
Cossimi <114231525+Cossimi@users.noreply.github.com>
2026-05-16 00:32:32 +0300

    Delete app directory

--- app/DTO/weather.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-#DTO - data transfer object
-class WeatherResponse(BaseModel):
-    city: str
-    temperature: float
-    description: str
-    humidity: int
--- app/main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from app.api import weather_router
-
-app = FastAPI(title="Weather API")
-app.include_router(weather_router.router)
